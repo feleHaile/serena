72f218ee08c42326266fc340c2f69b12848c2ced
eudesyawog <gyjeaneudes@gmail.com>
2018-07-03 10:58:49 +0000

    LLeroux 2 end of corrections

--- plot/influence_multisource.R
+++ plot/influence_multisource.R
@@ -4,32 +4,41 @@ library(ggthemes)
 library(foreach)
 library(scales)
 
-ndvi <- read.csv("/media/je/JE3/to_plot/Plot_CSV/agg_NDVI_original_PRS_toggplot.csv",colClasses = 
+ndvi <- read.csv("/media/je/JE3/R_plot/Plot_CSV/agg_NDVI_original_PRS_toggplot.csv",colClasses = 
                    c("Date","character","character","character","character","Date","Date","double","double"))
 ndvi$CropSys[ndvi$CropSys=="Mixed"] <- "Mixte"
+ndvi$CropSys[ndvi$CropSys=="Pure"] <- "Pur"
 ndvi <- ndvi[complete.cases(ndvi), ]
 ndvi$index <- "NDVI"
 
-msavi2 <- read.csv("/media/je/JE3/to_plot/Plot_CSV/agg_MSAVI2_original_PRS_toggplot.csv",colClasses = 
+msavi2 <- read.csv("/media/je/JE3/R_plot/Plot_CSV/agg_MSAVI2_original_PRS_toggplot.csv",colClasses = 
                      c("Date","character","character","character","character","Date","Date","double","double"))
 msavi2$CropSys[msavi2$CropSys=="Mixed"] <- "Mixte"
+msavi2$CropSys[msavi2$CropSys=="Pure"] <- "Pur"
 msavi2 <- msavi2[complete.cases(msavi2), ]
 msavi2$index <- "MSAVI2"
 
 id <- unique(ndvi$ID)
 
-ndvi_subset <- subset(ndvi,ndvi$ID=="MAEE3"|ndvi$ID=="MAEE2"|ndvi$ID=="P16"|ndvi$ID=="P12")
+ndvi_subset <- subset(ndvi,ndvi$ID=="AMES1"|ndvi$ID=="AMSF1"|ndvi$ID=="JA1"|ndvi$ID=="MAEE3"|ndvi$ID=="MMFF1"|ndvi$ID=="P16")
 ndvi_subset$Systeme <- paste(ndvi_subset$Culture,ndvi_subset$CropSys, sep=" ")
 
 ###########"Facets"###########
-p <- ggplot(data=ndvi_subset, aes(x=Date, y=Avg)) + geom_point(aes(shape = Sensor, color = Systeme)) + geom_line(aes(color = Systeme)) + 
-  geom_vline(xintercept = unique(plot$Semi), color="darkorange", size=1.2)+geom_vline(xintercept = unique(plot$Recolte), color="mediumorchid1", size=1.2) +
-  facet_wrap (~ ID, ncol = 3)
+p <- ggplot(data=ndvi_subset, aes(x=Date, y=Avg)) + geom_line(aes(color = Systeme)) + 
+  geom_vline(aes(xintercept = Semi), colour="grey29", size=1) + #maroon3
+  geom_vline(aes(xintercept = Recolte),  colour="grey29", size=1) + #darkorchid3
+  geom_point(aes(shape = Sensor,color = Systeme)) +
+  geom_ribbon(aes(ymin=Avg-Std,ymax=Avg+Std, fill=Systeme),alpha=0.2)+
+  facet_wrap (~ ID, ncol = 3) + theme_bw() +
+  #ggtitle("Moyenne parcellaire de NDVI et Ecart type") + theme(plot.title = element_text(hjust = 0.5)) +
+  scale_x_date(name="Date", breaks = date_breaks("2 months"),labels = date_format("%b"), limits = c(as.Date("2017-05-08"),as.Date("2017-11-19"))) +
+  ylab("Valeur") + 
+  labs(shape = "Capteurs", color = "Culture et Système", fill="Culture et Système")
 
-p
+ggsave("/home/je/Bureau/serena/report/materiels_methodes/multisource.png")
 ##############################
 
-pdf("/media/je/JE3/to_plot/Plot_PDF_PNG/test.pdf", width = 8.98, height = 5.86, title = "Moyenne Parcellaire NDVI")
+pdf("/media/je/JE3/R_plot/Plot_PDF_PNG/test.pdf", width = 8.98, height = 5.86, title = "Moyenne Parcellaire NDVI")
 
 foreach(i=1:length(id)) %do% {
   plot <- ndvi[which(ndvi$ID==id[i]),]
@@ -45,7 +54,7 @@ foreach(i=1:length(id)) %do% {
 }
 dev.off()
 
-pdf("/media/je/JE3/to_plot/Plot_PDF_PNG/test2.pdf", width = 8.98, height = 5.86, title = "Moyenne Parcellaire MSAVI2")
+pdf("/media/je/JE3/R_plot/Plot_PDF_PNG/test2.pdf", width = 8.98, height = 5.86, title = "Moyenne Parcellaire MSAVI2")
 
 foreach(i=1:length(id)) %do% {
   plot <- msavi2[which(msavi2$ID==id[i]),]
@@ -62,7 +71,7 @@ foreach(i=1:length(id)) %do% {
 }
 dev.off()
 
-pdf("/media/je/JE3/to_plot/Plot_PDF_PNG/test.pdf", width = 8.98, height = 5.86, title = "Moyenne Parcellaire NDVI et MSAVI2")
+pdf("/media/je/JE3/R_plot/Plot_PDF_PNG/test.pdf", width = 8.98, height = 5.86, title = "Moyenne Parcellaire NDVI et MSAVI2")
 
 foreach(i=1:length(id)) %do% {
   plot1 <- ndvi[which(ndvi$ID==id[i]),]
